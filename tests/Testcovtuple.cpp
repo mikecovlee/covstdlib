be56482763bba6b87b1b6c93fcbd3ef537f4f419
李登淳 <mikecovlee@163.com>
2016-08-20 16:15:45 +0800

    Remove Files

--- tests/Testcovtuple.cpp
+++
@@ -1,8 +0,0 @@
-#include "covstdlib.h"
-#include <cstdio>
-#include <string>
-int main()
-{
-	auto t = cov::make_tuple(3.1415926, std::string("Hello"), 22);
-	printf("%s", t.get < std::string > ().c_str());
-}
--- tests/Testfindex.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "covstdlib.h"
-int main()
-{
-	return cov::make_function_index([&] {
-		std::cout<<"Hello,world!"<<std::endl;
-		return 0;
-	})();
-}
