08a8e669ac27204f814d3fd180365fb80ff82a2a
Mike Lee <mikecovlee@163.com>
2016-09-26 20:46:29 +0800

    添加cov::any测试单元

---
+++ tests/testcovany.cpp
@@ -0,0 +1,21 @@
+#include "covany.hpp"
+#include <iostream>
+#include <cmath>
+int main()
+{
+	cov::any var;
+	var=var.usable();
+	if(!var)
+		std::cout<<"Ok"<<std::endl;
+	if(var!=12345)
+		std::cout<<"Ok"<<std::endl;
+	var=3.1415926;
+	var=std::pow(static_cast<double>(var),static_cast<double>(var));
+	std::cout<<var<<std::endl;
+	const cov::any var1=var;
+	if(var==var1)
+		std::cout<<"Ok"<<std::endl;
+	std::cout<<var1<<std::endl;
+	var=0;
+	return var;
+}
