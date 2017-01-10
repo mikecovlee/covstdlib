7a1ffca1864e0dfa67340e46818ca0c0ecb75ec2
Mike Lee <mikecovlee@163.com>
2017-01-10 18:05:40 +0800

    更新部分细节

--- test_tlist.cpp
+++ test_tlist.cpp
@@ -3,10 +3,14 @@
 #include <cstdio>
 int main()
 {
-    using A=typename cov::make_tlist<void,void,float,void>::result;
+    using A=typename cov::make_tlist<int,float,char,bool>::result;
     printf("%d\n",cov::get_size<A>::result);
-    using B=typename cov::erase<3,A>::result;
+    cov::iterator<A>::show();
+    using B=typename cov::erase<1,A>::result;
     printf("%d\n",cov::get_size<B>::result);
-    printf("%s\n",typeid(B).name());
+    cov::iterator<B>::show();
+    using C=typename cov::push_back<B,double>::result;
+    printf("%d\n",cov::get_size<C>::result);
+    cov::iterator<C>::show();
     return 0;
 }
--- type_list.hpp
+++ type_list.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstdio>
+#include <typeinfo>
 namespace cov {
 	template<typename fT,typename sT>struct tlist_node {
 		using first=fT;
@@ -17,8 +19,21 @@ namespace cov {
 	template<typename fT,typename sT,int size>struct get_size<tlist_node<fT,sT>,size> {
 		static constexpr int result=get_size<sT,size+1>::result;
 	};
+	template<typename T>struct iterator {
+		static void show()
+		{
+			printf("%s\n",typeid(T).name());
+		}
+	};
+	template<typename fT,typename sT>struct iterator<tlist_node<fT,sT>> {
+		static void show()
+		{
+			printf("%s\t",typeid(fT).name());
+			iterator<sT>::show();
+		}
+	};
 	template<int posit,typename T>struct get_type {
-		using result=void;
+		//using result=void;
 	};
 	template<int posit,typename fT,typename sT>struct get_type<posit,tlist_node<fT,sT>> {
 		using result=typename get_type<posit-1,sT>::result;
@@ -30,7 +45,7 @@ namespace cov {
 		using result=fT;
 	};
 	template<int posit,typename T,typename Arg>struct change_type {
-		using result=void;
+		//using result=void;
 	};
 	template<int posit,typename fT,typename sT,typename Arg>struct change_type<posit,tlist_node<fT,sT>,Arg> {
 		using result=tlist_node<fT,typename change_type<posit-1,sT,Arg>::result>;
@@ -45,13 +60,13 @@ namespace cov {
         using result=tlist_node<Arg,T>;
     };
     template<typename T>struct pop_front{
-        using result=void;
+        //using result=void;
     };
     template<typename fT,typename sT>struct pop_front<tlist_node<fT,sT>>{
         using result=sT;
     };
 	template<typename T,typename Arg>struct push_back{
-		using result=void;	
+		//using result=void;	
 	};
 	template<typename fT,typename sT,typename Arg>struct push_back<tlist_node<fT,sT>,Arg>{
 		using result=tlist_node<fT,tlist_node<sT,Arg>>;
@@ -60,7 +75,7 @@ namespace cov {
 		using result=tlist_node<fT,typename push_back<tlist_node<sT,tT>,Arg>::result>;
 	};
 	template<typename T>struct pop_back{
-		using result=void;
+		//using result=void;
 	};
 	template<typename fT,typename sT>struct pop_back<tlist_node<fT,sT>>{
 		using result=fT;
@@ -69,16 +84,13 @@ namespace cov {
 		using result=tlist_node<fT,typename pop_back<tlist_node<sT,tT>>::result>;
 	};
 	template<int posit,typename T>struct erase{
-		using result=void;
+		//using result=void;
 	};
-	template<int posit,typename fT,typename sT,typename tT>struct erase<posit,tlist_node<fT,tlist_node<sT,tT>>>{
-		using result=tlist_node<fT,typename erase<posit-1,tlist_node<sT,tT>>::result>;
+	template<int posit,typename fT,typename sT>struct erase<posit,tlist_node<fT,sT>>{
+		using result=tlist_node<fT,typename erase<posit-1,sT>::result>;
 	};
 	template<typename fT,typename sT>struct erase<0,tlist_node<fT,sT>>{
 		using result=sT;
 	};
-	/*template<typename fT,typename sT>struct erase<1,tlist_node<fT,sT>>{
-		using result=fT;
-	};*/
-
+	template<int posit,typename T,typename Arg>
 }
