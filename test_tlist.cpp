190589474400618dd11dc480b802b86d810186bb
Mike Lee <mikecovlee@163.com>
2017-01-12 10:59:01 +0800

    更新使用新结构的beta2

--- test_tlist.cpp
+++ test_tlist.cpp
@@ -1,16 +1,19 @@
-#include "./type_list_beta1.hpp"
+#include "./type_list_beta2.hpp"
 #include <typeinfo>
 #include <cstdio>
 int main()
 {
 	using A=typename cov::make_tlist<int,float,char,bool>::result;
-	printf("%d\n",cov::get_size<A>::result);
+	printf("A:size=%d\t",cov::get_size<A>::result);
 	cov::iterator<A>::show();
-	using B=typename cov::erase<1,A>::result;
-	printf("%d\n",cov::get_size<B>::result);
+	printf("\n");
+	using B=typename cov::erase<2,A>::result;
+	printf("B:size=%d\t",cov::get_size<B>::result);
 	cov::iterator<B>::show();
+	printf("\n");
 	using C=typename cov::insert<2,A,double>::result;
-	printf("%d\n",cov::get_size<C>::result);
+	printf("C:size=%d\t",cov::get_size<C>::result);
 	cov::iterator<C>::show();
+	printf("\n");
 	return 0;
 }
---
+++ type_list_beta2.hpp
@@ -0,0 +1,81 @@
+#pragma once
+#include <cstdio>
+#include <typeinfo>
+namespace cov {
+	struct tlist_final {};
+	template<typename fT,typename sT>struct tlist_node {};
+	template<typename...>struct make_tlist;
+	template<typename T>struct make_tlist<T> {
+		using result=tlist_node<T,tlist_final>;
+	};
+	template<typename T,typename...Args>struct make_tlist<T,Args...> {
+		using result=tlist_node<T,typename make_tlist<Args...>::result>;
+	};
+	template<typename T,int size=0>struct get_size;
+	template<int size>struct get_size<tlist_final,size>{
+		static constexpr int result=size;
+	};
+	template<typename fT,typename sT,int size>struct get_size<tlist_node<fT,sT>,size> {
+		static constexpr int result=get_size<sT,size+1>::result;
+	};
+	template<typename T>struct iterator;
+	template<>struct iterator<tlist_final>{
+		static void show(){}
+	};
+	template<typename fT,typename sT>struct iterator<tlist_node<fT,sT>> {
+		static void show()
+		{
+			printf("%s\t",typeid(fT).name());
+			iterator<sT>::show();
+		}
+	};
+	template<int posit,typename T>struct get_type;
+	template<int posit,typename fT,typename sT>struct get_type<posit,tlist_node<fT,sT>> {
+		using result=typename get_type<posit-1,sT>::result;
+	};
+	template<typename fT,typename sT>struct get_type<0,tlist_node<fT,sT>> {
+		using result=fT;
+	};
+	template<int posit,typename T,typename Arg>struct change_type;
+	template<int posit,typename fT,typename sT,typename Arg>struct change_type<posit,tlist_node<fT,sT>,Arg> {
+		using result=tlist_node<fT,typename change_type<posit-1,sT,Arg>::result>;
+	};
+	template<typename fT,typename sT,typename Arg>struct change_type<0,tlist_node<fT,sT>,Arg> {
+		using result=tlist_node<Arg,sT>;
+	};
+	template<typename T,typename Arg>struct push_front {
+		using result=tlist_node<Arg,T>;
+	};
+	template<typename T>struct pop_front;
+	template<typename fT,typename sT>struct pop_front<tlist_node<fT,sT>> {
+		using result=sT;
+	};
+	template<typename T,typename Arg>struct push_back;
+	template<typename fT,typename sT,typename Arg>struct push_back<tlist_node<fT,sT>,Arg> {
+		using result=tlist_node<fT,typename push_back<sT,Arg>::result>;
+	};
+	template<typename T,typename Arg>struct push_back<tlist_node<T,tlist_final>,Arg>{
+		using result=tlist_node<T,tlist_node<Arg,tlist_final>>;
+	};
+	template<typename T>struct pop_back;
+	template<typename fT,typename sT>struct pop_back<tlist_node<fT,sT>> {
+		using result=tlist_node<fT,typename pop_back<sT>::result>;
+	};
+	template<typename T>struct pop_back<tlist_node<T,tlist_final>> {
+		using result=tlist_final;
+	};
+	template<int posit,typename T>struct erase;
+	template<int posit,typename fT,typename sT>struct erase<posit,tlist_node<fT,sT>> {
+		using result=tlist_node<fT,typename erase<posit-1,sT>::result>;
+	};
+	template<typename fT,typename sT>struct erase<0,tlist_node<fT,sT>> {
+		using result=sT;
+	};
+	template<int posit,typename T,typename Arg>struct insert;
+	template<int posit,typename fT,typename sT,typename Arg>struct insert<posit,tlist_node<fT,sT>,Arg> {
+		using result=tlist_node<fT,typename insert<posit-1,sT,Arg>::result>;
+	};
+	template<typename fT,typename sT,typename Arg>struct insert<0,tlist_node<fT,sT>,Arg> {
+		using result=tlist_node<fT,tlist_node<Arg,sT>>;
+	};
+}
